6794260b424ccfb5413ddebd516ea93fa04d65dc
Devin Sit <devin@devinsit.com>
2020-03-29 16:11:26 -0400

    KBA-58 Updated template Node Dockerfile versions to the latest 12.16.1 LTS.
    Changelog:
    
    - Users can now make use of the latest Node 12.16.1 LTS for the template Dockerfiles.
    
    Implementation Details:
    
    - As a result of the latest Node LTS image having an updated version of BusyBox, the 'retryable-db-migrate' script had to be fixed to use tha latest BusyBox's syntax for 'timeout'.

--- kubails/templates/backend-express/{{cookiecutter.name}}/scripts/retryable-db-migrate.sh
+++ kubails/templates/backend-express/{{cookiecutter.name}}/scripts/retryable-db-migrate.sh
@@ -5,7 +5,10 @@ n=0
 # Tries up to 5 times to perform the migrations
 until [ $n -ge 5 ]
 do
-    timeout -t 5 npm run db:migrate && break
+    # Note that this 'timeout' is BusyBox's latest version of 'timeout',
+    # which uses a different syntax for specifying the time.
+    # Bash uses `timeout -t 5`, BusyBox uses `timeout 5`.
+    timeout 5 npm run db:migrate && break
     ((n+=1))
     sleep 2
 done
