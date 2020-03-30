99945abdc4dfe7ce37904b83a14bcbf591385b16
Devin Sit <devin@devinsit.com>
2020-03-30 00:16:47 -0400

    KBA-61 Added minimally functional support for Terraform 0.12. (#21)
    Changelog:
    
    - Users can now use Kubails with Terraform 0.12 (tested with 0.12.24).
    - Note: Using Terraform 0.12 will result in deprecation warnings related to variable interpolation; the Terraform configs have yet to be updated to be fully 0.12 compliant in order to maintain backwards compatibility with 0.11.
    
    Implementation Details:
    
    - Had to switch to passing `kubails.json` values to Terraform using `TF_VAR` instead `-var` options, because of a change in Terraform 0.12 requiring all passed `-var` options to be declared variables (this restriction does not apply to `TF_VAR` env variables).
    - Had to change how `kubails.json` values are stringified for Terraform, since Terraform 0.12 changed value parsing to now parse double quotes literally (but only for non-map, non-list values).
    - As a result of the first implementation detail, the `get_command_output` helper now takes `kwargs`. If anything, this was just an oversight since `call_command` already accepted them.

--- kubails/external_services/terraform.py
+++ kubails/external_services/terraform.py
@@ -77,51 +77,103 @@ class Terraform:
         return result
 
     def run_command(self, subcommand: str, arguments: List[str] = [], with_vars=True) -> bool:
-        command = self.base_command + [subcommand]
+        command = self.base_command + [subcommand] + arguments
+        var_options = self._convert_config_to_var_options(self.variables) if self.variables and with_vars else None
 
-        if self.variables and with_vars:
-            command.extend(self._convert_config_to_var_options(self.variables))
-
-        command.extend(arguments)
-        return self._run_terraform_command(command)
+        return self._run_terraform_command(command, env_vars=var_options)
 
     def _run_terraform_command(
         self,
         command: List[str],
-        call_function: Callable[..., Any] = call_command
+        call_function: Callable[..., Any] = call_command,
+        env_vars: Dict[str, str] = None
     ) -> Any:
         """
+        Runs a Terraform command.
+
         Since some Terraform commands don't take a folder as an argument, we have to
         make sure that all commands are run inside the 'terraform' folder.
+
+        Note for @param env_vars:
+
+            The default/empty value _must_ None. If the default/empty value is just an empty dict
+            (or otherwise), then this can cause problems with commands expected normal environment
+            variables.
+
+            This is noted here: https://docs.python.org/2/library/subprocess.html#popen-constructor
+            "If specified, env must provide any variables required for the program to execute."
+
+            This first came up when trying to run `terraform init` with an empty dict.
+            When installing the google provider, it would throw this error:
+            "Error installing provider "google": exec: "getent": executable file not found in $PATH."
+
+            This error is a direct result of providing an empty env dict and triggering a loss
+            of other environment variables for commands.
+
+            Whether or not this will affect other commands that need TF_VAR env variables is unknown at the moment.
         """
         current_dir = os.getcwd()
         os.chdir(os.path.join(self.root_folder, TERRAFORM_FOLDER))
 
-        result = call_function(command, shell=True)
+        result = call_function(command, shell=True, env=env_vars)
         os.chdir(current_dir)
 
         return result
 
-    def _convert_config_to_var_options(self, config: Dict[str, Any]) -> List[str]:
-        var_options = []
+    def _convert_config_to_var_options(self, config: Dict[str, Any]) -> Dict[str, str]:
+        var_options = {}
 
         for key, value in config.items():
-            var_options.append("-var")
-            var_options.append("'{}={}'".format(key, self._stringify_value(value)))
+            # We originally used '-var' options and just appended them to the Terraform command.
+            #
+            # However, Terraform 0.12 changed so that it would throw an error whenever it received
+            # a '-var' option for an undeclared variable (i.e. it didn't exist in variables.tf).
+            #
+            # This is quite problematic for us since Kubails depends on being able to just dump
+            # kubails.json to Terraform without worrying about what has been declared or not.
+            #
+            # As a result, we have switched to passing values using the "TF_VAR" env var method.
+            #
+            # This problem, and solution, are documented here: https://github.com/hashicorp/terraform/issues/22004.
+            var_options["TF_VAR_{}".format(key)] = self._stringify_value(value)
 
         return var_options
 
-    def _stringify_value(self, value: Any) -> str:
+    def _stringify_value(self, value: Any, top_level: bool = True) -> str:
+        # (OK, this is really stupid, but I couldn't think of a better solution, so...)
+        #
+        # 'top_level' is a flag that indicates whether _stringify_value has been called recursively or not.
+        #
+        # The reason we need this is because Terraform 0.12 changed how '-var' and 'TF_VAR' args are passed.
+        # Now, when it receives an arg like `-var '_project_name="name"'`, it interprets the double quotes literally.
+        # This is noted here: https://www.terraform.io/docs/configuration/variables.html#complex-typed-values.
+        #
+        # That is, when interpolating the variable like `"${_project_name}-bucket"`, it literally outputs
+        # `""name"-bucket"`, with the double quotes included. Obviously, this then breaks the parsing
+        # of the whole string, since it will seem (to Terraform) to be multiple strings.
+        #
+        # However, this literal quoting only happens with non-map, non-list values. Because the values
+        # in a list or map still need to be quoted properly... but it won't take those as literal quotes.
+        #
+        # See here for some valid Terraform 0.12 examples:
+        # https://www.terraform.io/docs/configuration/variables.html#variables-on-the-command-line
+        #
+        # WHY it does this now, I have no clue.
+        #
+        # As such, when _stringify_value isn't called recursively (i.e. at 'top_level'), we _don't_ want
+        # to wrap values in double quotes, but we _do_ when it _is_ called recursively (i.e. not at 'top_level').
+
         if isinstance(value, dict):
             return self._stringify_dict(value)
         elif isinstance(value, list):
             return self._stringify_list(value)
         elif isinstance(value, bool):
-            # Terraform only interprets 'true' or 'false', not 'True' or 'False';
-            # thus, the string needs to be downcased.
-            return "\"{}\"".format(str(value).lower())
+            # Terraform only interprets 'true' or 'false', not 'True' or 'False'; thus, it needs to be lowercase.
+            stringified_value = str(value).lower()
+
+            return "\"{}\"".format(stringified_value) if not top_level else stringified_value
         elif isinstance(value, str) or isinstance(value, Number):
-            return "\"{}\"".format(value)
+            return "\"{}\"".format(value) if not top_level else "{}".format(value)
         elif value is None:
             return "\"\""
         else:
@@ -129,14 +181,14 @@ class Terraform:
 
     def _stringify_dict(self, dict_to_convert: Dict[str, Any]) -> str:
         return "{{{}}}".format(reduce(
-            lambda acc, pair: "{},{}={}".format(acc, pair[0], self._stringify_value(pair[1])),
+            lambda acc, pair: "{},{}={}".format(acc, pair[0], self._stringify_value(pair[1], top_level=False)),
             dict_to_convert.items(),
             ""
         )[1:])  # Remove the (useless) leading comma
 
     def _stringify_list(self, list_to_convert: List[str]) -> str:
         return "[{}]".format(reduce(
-            lambda acc, item: "{},{}".format(acc, self._stringify_value(item)),
+            lambda acc, item: "{},{}".format(acc, self._stringify_value(item, top_level=False)),
             list_to_convert,
             ""
         )[1:])  # Remove the (useless) leading comma
