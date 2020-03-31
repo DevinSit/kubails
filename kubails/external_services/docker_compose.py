195a33fda2d1ae0c06f0cc46ea6a56d71154e295
Devin Sit <devin@devinsit.com>
2020-03-31 17:05:17 -0400

    KBA-33 Fixed generated services potentially having conflicting docker-compose ports. (#27)
    Changelog:
    
    - Users can now use `kubails service generate` without having to worry about the new generated service having a conflicting `docker-compose` port with an existing service. 
    
    Implementation Details:
    
    - The new service will have their host ports incremented until they no longer conflict with existing services.

--- kubails/external_services/docker_compose.py
+++ kubails/external_services/docker_compose.py
@@ -20,7 +20,7 @@ yaml.add_representer(type(None), yaml_represent_none)
 
 
 class DockerCompose:
-    def __init__(self, project_name: str, compose_folder: str) -> None:
+    def __init__(self, project_name: str = "", compose_folder: str = "") -> None:
         self.compose_file_location = os.path.join(compose_folder, COMPOSE_FILE)
         self.base_command = ["docker-compose", "-p", project_name, "--file", self.compose_file_location]
 
@@ -39,6 +39,7 @@ class DockerCompose:
         if not config["services"]:
             config["services"] = {}
 
+        self._fix_conflicting_ports(config["services"], new_service_config)
         config["services"].update(new_service_config)
 
         self._write_config(config)
@@ -69,3 +70,39 @@ class DockerCompose:
             except yaml.YAMLError as e:
                 logger.exception("Failed to write docker-compose config: " + str(e))
                 raise click.Abort()
+
+    def _fix_conflicting_ports(self, services_config: Dict[str, Any], new_service_config: Dict[str, Any]) -> None:
+        existing_ports = self._get_all_service_ports(services_config)
+
+        for name, config in new_service_config.items():
+            if "ports" in config:
+                fixed_ports = []
+
+                for ports in config["ports"]:
+                    # Remember, ports are specified in the "HOST:CONTAINER" format.
+                    # So the first port in the split is what we have to make sure doesn't conflict.
+                    split_ports = ports.split(":")
+                    host_port = split_ports[0]
+                    container_port = split_ports[1]
+
+                    # Increment the host port until it no longer conflicts.
+                    while host_port in existing_ports:
+                        host_port = str(int(host_port) + 1)
+
+                    # Add the fixed host port to existing ports so that subsequent
+                    # new_service_config ports don't conflict with it.
+                    existing_ports.append(host_port)
+
+                    fixed_ports.append("{}:{}".format(host_port, container_port))
+
+                new_service_config[name]["ports"] = fixed_ports
+
+    def _get_all_service_ports(self, services_config: Dict[str, Any]) -> List[str]:
+        all_ports = []
+
+        for config in services_config.values():
+            if "ports" in config:
+                for ports in config["ports"]:
+                    all_ports.append(ports.split(":")[0])
+
+        return all_ports
